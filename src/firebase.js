27e2cd5df28b4be78ec2eb9a6d4681cebe26f184
krishna <krishnawaghmode1995@gmail.com>
2022-10-11 23:41:02 +0530

    add .env

--- src/firebase.js
+++ src/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyA4_rL9FQ9KZjJixC75qRPyNeXrCOe5gTM',
-  authDomain: 'reactjs--login.firebaseapp.com',
-  projectId: 'reactjs--login',
-  storageBucket: 'reactjs--login.appspot.com',
-  messagingSenderId: '852197895770',
-  appId: '1:852197895770:web:285670223daee493b23263',
-  measurementId: 'G-4N35EW23GP',
+  apiKey: process.env.APIKEY,
+  authDomain:  process.env.AUTHDOMAIN,
+  projectId:  process.env.PROJECTID,
+  storageBucket:  process.env.STORAGEBUCKET,
+  messagingSenderId:  process.env.MESSAGINGSENDERID,
+  appId:  process.env.APPID,
+  measurementId:  process.env.MEASUREMENTID,
 };
 
 // Initialize Firebase
